d8b2846171166833aba243813cfe35d7226b980d
Shiyue00 <94933592+Shiyue00@users.noreply.github.com>
2026-01-20 11:46:09 +0800

    Fix aspect ratio entries in generation_handler.py

--- src/services/generation_handler.py
+++ src/services/generation_handler.py
@@ -137,7 +137,7 @@ MODEL_CONFIG = {
         "type": "video",
         "video_type": "i2v",
         "model_key": "veo_3_1_i2v_s_fast_portrait_fl_ultra_relaxed",
-        "aspect_ratio": "VIDEO_ASPECT_RATIO_PORTRAIT",
+        "aspect_ratio": "VIDEO_ASPECT_RATIO_PORTRAIT",  
         "supports_images": True,
         "min_images": 1,
         "max_images": 2
@@ -177,7 +177,7 @@ MODEL_CONFIG = {
         "type": "video",
         "video_type": "i2v",
         "model_key": "veo_2_0_i2v",
-        "aspect_ratio": "VIDEO_ASPECT_RATIO_PORTRAIT",
+        "aspect_ratio": "VIDEO_ASPECT_RATIO_PORTRAIT",  
         "supports_images": True,
         "min_images": 1,
         "max_images": 2
@@ -193,7 +193,7 @@ MODEL_CONFIG = {
     },
 
     # veo_3_1_i2v_s_fast_ultra (需要新增横竖屏)
-    "veo_3_1_i2v_s_fast_ultra_portrait": {  
+    "veo_3_1_i2v_s_fast_ultra_portrait": {
         "type": "video",
         "video_type": "i2v",
         "model_key": "veo_3_1_i2v_s_fast_portrait_ultra",
@@ -202,7 +202,7 @@ MODEL_CONFIG = {
         "min_images": 1,
         "max_images": 2
     },
-    "veo_3_1_i2v_s_fast_ultra_landscape": {  
+    "veo_3_1_i2v_s_fast_ultra_landscape": {
         "type": "video",
         "video_type": "i2v",
         "model_key": "veo_3_1_i2v_s_fast_landscape_ultra",
